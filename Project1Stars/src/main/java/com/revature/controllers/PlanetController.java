e7d9fa5c15f06b668eee975e4d28c173ec064834
Viktor <vpisarenko23@my.whitworth.edu>
2024-04-15 14:02:48 -0700

    added more planet methods to controller

--- Project1Stars/src/main/java/com/revature/controllers/PlanetController.java
+++ Project1Stars/src/main/java/com/revature/controllers/PlanetController.java
@@ -2,6 +2,7 @@ package com.revature.controllers;
 
 import com.revature.daos.PlanetDAO;
 import com.revature.models.Planet;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,9 +27,26 @@ public class PlanetController {
         return ResponseEntity.ok(allPlanets);
     }
 
+    @GetMapping("{planetId}")
+    public ResponseEntity<Planet> getPlanetById(@PathVariable int planetId){
+        return ResponseEntity.ok(planetDAO.findById(planetId).get());
+    }
+
+
     @PostMapping
     public ResponseEntity<Planet> addPlanet(@RequestBody Planet planet){
         return ResponseEntity.accepted().body(planetDAO.save(planet));
     }
 
+    @PutMapping
+    public ResponseEntity<Planet> updatePlanet(@RequestBody Planet planet){
+        return ResponseEntity.ok(planetDAO.save(planet));
+    }
+
+    @DeleteMapping("{planetId}")
+    public ResponseEntity<String> deletePlanet(@PathVariable int planetId){
+        Planet p = planetDAO.findById(planetId).get();
+        planetDAO.deleteById(planetId);
+        return ResponseEntity.accepted().body("Planet " + p.getName() + " has been deleted");
+    }
 }
