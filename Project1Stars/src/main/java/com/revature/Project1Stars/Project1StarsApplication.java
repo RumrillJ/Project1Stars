55a7c0fde250f9bb225bec10abde9e652b8edc45
rumrillj <john.rumrill@hotmail.com>
2024-04-15 07:46:13 -0700

    Initial commit

---
+++ Project1Stars/src/main/java/com/revature/Project1Stars/Project1StarsApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Project1Stars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1StarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1StarsApplication.class, args);
+	}
+
+}
